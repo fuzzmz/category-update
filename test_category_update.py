ca64bc98b7333f18e4da0ef4f76c39f2798f01a7
Serban Constantin <serban.constantin@freescale.com>
2015-02-09 11:50:55 +0200

    fix paths for test files

--- test_category_update.py
+++ test_category_update.py
@@ -6,9 +6,10 @@ from category_update import normalize, get_features, get_versions
 
 @pytest.fixture
 def set_fixture():
-    features = get_features(os.path.realpath(__file__).rsplit('\\', 1)[0] + '/tests/category.xml')
-    path = os.path.realpath(__file__).rsplit('\\', 1)[0] + '/tests/features/'
-    return features, path
+    root, script = os.path.split(os.path.realpath(__file__))
+    features = get_features(root + '/tests/category.xml')
+    path = root + '/tests/features/'
+    return features, path, root
 
 
 def test_normalize():
@@ -19,13 +20,15 @@ def test_normalize_nonascii():
     assert normalize(u"testá") == 'test'
 
 
-def test_get_features():
-    assert get_features(os.path.realpath(__file__).rsplit('\\', 1)[0] + '/tests/category.xml') == [
-        'com.fuzzmz_testfeature4.ext', 'com.fuzzmz.testfeature2', 'com.fuzzmz.testfeature3_ext', 'com.fuzzmz_testfeature1']
+def test_get_features(set_fixture):
+    features, path, root = set_fixture
+    assert get_features(root + '/tests/category.xml') == [
+        'com.fuzzmz_testfeature4.ext', 'com.fuzzmz.testfeature2', 'com.fuzzmz.testfeature3_ext',
+        'com.fuzzmz_testfeature1']
 
 
 def test_get_versions(set_fixture):
-    features, path = set_fixture
+    features, path, root = set_fixture
     assert get_versions(features, path) == [('com.fuzzmz_testfeature4.ext', '4.8.0.207352_201406-060126'),
                                             ('com.fuzzmz.testfeature2', '3.8.0.207352-201406060126'),
                                             ('com.fuzzmz.testfeature3_ext', '4.8.0.207352_201406060126'),
