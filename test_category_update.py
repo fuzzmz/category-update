ba8d567378e724551628bb460b6444ecdb7d9394
Serban Constantin <serban.constantin@gmail.com>
2015-02-09 13:02:00 +0200

    set features list and feature-version mapping as fixtures

--- test_category_update.py
+++ test_category_update.py
@@ -7,9 +7,15 @@ from category_update import normalize, get_features, get_versions
 @pytest.fixture
 def set_fixture():
     root, script = os.path.split(os.path.realpath(__file__))
-    features = get_features(root + '/tests/category.xml')
+    getfeaturesfunc = get_features(root + '/tests/category.xml')
     path = root + '/tests/features/'
-    return features, path, root
+    features = ['com.fuzzmz_testfeature1', 'com.fuzzmz.testfeature2', 'com.fuzzmz.testfeature3_ext',
+                'com.fuzzmz_testfeature4.ext']
+    featuremap = [('com.fuzzmz_testfeature4.ext', '4.8.0.207352_201406-060126'),
+                  ('com.fuzzmz.testfeature2', '3.8.0.207352-201406060126'),
+                  ('com.fuzzmz.testfeature3_ext', '4.8.0.207352_201406060126'),
+                  ('com.fuzzmz_testfeature1', '2.8.0.207352-201406060126')]
+    return getfeaturesfunc, path, root, features, featuremap
 
 
 def test_normalize():
@@ -21,14 +27,10 @@ def test_normalize_nonascii():
 
 
 def test_get_features(set_fixture):
-    features, path, root = set_fixture
-    assert get_features(root + '/tests/category.xml') == ['com.fuzzmz_testfeature1', 'com.fuzzmz.testfeature2',
-                                                          'com.fuzzmz.testfeature3_ext', 'com.fuzzmz_testfeature4.ext']
+    getfeaturesfunc, path, root, features, featuremap = set_fixture
+    assert get_features(root + '/tests/category.xml') == features
 
 
 def test_get_versions(set_fixture):
-    features, path, root = set_fixture
-    assert get_versions(features, path) == [('com.fuzzmz_testfeature4.ext', '4.8.0.207352_201406-060126'),
-                                            ('com.fuzzmz.testfeature2', '3.8.0.207352-201406060126'),
-                                            ('com.fuzzmz.testfeature3_ext', '4.8.0.207352_201406060126'),
-                                            ('com.fuzzmz_testfeature1', '2.8.0.207352-201406060126')]
+    getfeaturesfunc, path, root, features, featuremap = set_fixture
+    assert get_versions(getfeaturesfunc, path) == featuremap
